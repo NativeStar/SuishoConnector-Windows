7846f0b616855d39f453ef201b14869380bdfd84
NativeStar <z2858470935@163.com>
2025-11-24 23:25:15 +0800

    修复高度异常

--- renderer/src/components/AppBar.tsx
+++ renderer/src/components/AppBar.tsx
@@ -3,7 +3,7 @@ interface AppBarProps {
 }
 export function AppBar({paddingLeft}:AppBarProps) {
     return (
-        <div style={{paddingLeft}} className="text-[#a9a9a9] title-bar mdui-theme-auto min-w-[90%] min-h-[8%] bg-[#fdf7fe] -mt-1.5 pt-3.5 ml-[-1%] z-2301 fixed top-0 left-0">
+        <div style={{paddingLeft}} className="text-[#a9a9a9] title-bar mdui-theme-auto min-w-[90%] min-h-[7.7%] bg-[#fdf7fe] -mt-1.5 pt-3.5 ml-[-1%] z-2301 fixed top-0 left-0">
             Suisho Connector
         </div>
     )
