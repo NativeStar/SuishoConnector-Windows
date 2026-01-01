1496bf0e5ce925c542af22a2b9cc4d88a5cc00ea
NativeStar <z2858470935@163.com>
2026-01-01 17:19:32 +0800

    彻底移除旧assets 更新prompt

--- assets.old/js/class/NotificationProfilePanel.js
+++
@@ -1,9 +0,0 @@
-class NotificationProfilePanel{
-    static ProfileTipsMaskText={
-        DISABLED:`已禁用对该应用通知的处理
-        来自其的通知将不会被记录及转发`,
-        PROFILE_DISABLED:`已关闭单独配置
-        对其通知的处理方式将遵循全局配置`
-    }
-}
-export {NotificationProfilePanel}
--- assets.old/js/class/TrustMode.js
+++
@@ -1,6 +0,0 @@
-class TrustMode{
-    static TRUST_UNSELECT=0;
-    static TRUST_TRUSTED=1;
-    static TRUST_UNTRUSTED=2;
-}
-export default TrustMode
