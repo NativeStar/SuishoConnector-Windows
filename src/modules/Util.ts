42012a13273f251734cc4ce0d0307b66fa0200e5
NativeStar <z2858470935@163.com>
2026-01-08 18:49:10 +0800

    修复开发模式判断

--- src/modules/Util.ts
+++ src/modules/Util.ts
@@ -10,7 +10,6 @@ import os from "os";
 import forge from "node-forge"
 type Config = typeof configTemp;
 class Util {
-    private static Developing = true;
     //Windows文件名保留字
     private static windowsReservedWords = new Set(["CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]);
     //url判断正则
@@ -72,7 +71,7 @@ class Util {
      * @memberof Util
      */
     static get isDeveloping(): boolean {
-        return this.Developing;
+        return !app.isPackaged;
     }
     /**
      * @static
