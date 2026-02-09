f04bd8cab2b85858f54e51f5ed2f8a205d4e53be
NativeStar <z2858470935@163.com>
2026-02-09 21:28:06 +0800

    当拖动文件到导航轨道上时可切换标签页

--- renderer/src/pages/Home/components/NavigationRail.tsx
+++ renderer/src/pages/Home/components/NavigationRail.tsx
@@ -5,11 +5,12 @@ import "mdui/components/button-icon"
 import "mdui/components/badge"
 import { type PageRouteProps } from "./PageRoute";
 import type { NavigationRail as MduiNavigationRail } from "mdui/components/navigation-rail"
+import type { NavigationRailItem as MduiNavigationRailItem } from "mdui/components/navigation-rail-item"
 interface NavigationRailProps {
     onChange: (value: PageRouteProps["page"]) => void,
-    hasNewTransmitMessage:boolean,
-    hasNewNotification:boolean,
-    value:PageRouteProps["page"]
+    hasNewTransmitMessage: boolean,
+    hasNewNotification: boolean,
+    value: PageRouteProps["page"]
 }
 
 export default function NavigationRail({
@@ -17,27 +18,26 @@ export default function NavigationRail({
     hasNewTransmitMessage,
     hasNewNotification,
     value,
-}:NavigationRailProps) {
+}: NavigationRailProps) {
+    function onRailItemDragEnterHandle(event: React.DragEvent<HTMLDivElement>) {
+        (event.target as MduiNavigationRailItem).click();
+    }
     return (
-        <mdui-navigation-rail value={value} className="w-[10%] h-[91.5%] top-[8%] navigationRailIndex" onClick={event=>onChange((event.target as MduiNavigationRail)!.value as PageRouteProps["page"])}>
-            {/* 未来改为功能菜单键 */}
-            {/* <mdui-tooltip content="设置" slot="bottom">
-                <mdui-button-icon icon=""/>
-            </mdui-tooltip> */}
+        <mdui-navigation-rail value={value} className="w-[10%] h-[91.5%] top-[8%] navigationRailIndex" onClick={event => onChange((event.target as MduiNavigationRail)!.value as PageRouteProps["page"])}>
             {/* 逆天替ui库修bug */}
-            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="home" value="home">主页</mdui-navigation-rail-item>
-            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="import_export" value="transmit">
+            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="home" value="home" onDragEnter={onRailItemDragEnterHandle}>主页</mdui-navigation-rail-item>
+            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="import_export" value="transmit" onDragEnter={onRailItemDragEnterHandle}>
                 数据互传
-                {hasNewTransmitMessage&&<mdui-badge slot="badge" variant="small"/>}
+                {hasNewTransmitMessage && <mdui-badge slot="badge" variant="small" />}
             </mdui-navigation-rail-item>
-            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="notifications" value="notification">
+            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="notifications" value="notification" onDragEnter={onRailItemDragEnterHandle}>
                 通知转发
-                {hasNewNotification&&<mdui-badge slot="badge" variant="small"/>}
+                {hasNewNotification && <mdui-badge slot="badge" variant="small" />}
             </mdui-navigation-rail-item>
-            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="folder_open" value="file">
+            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="folder_open" value="file" onDragEnter={onRailItemDragEnterHandle}>
                 文件浏览
             </mdui-navigation-rail-item>
-            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="settings" value="setting">
+            <mdui-navigation-rail-item className="-ml-2 pl-[11.8%] w-[60%] whitespace-nowrap" icon="settings" value="setting" onDragEnter={onRailItemDragEnterHandle}>
                 设置
             </mdui-navigation-rail-item>
         </mdui-navigation-rail>
