648ae7d82cc0dff2e7d46af0874c4c407f3ed2bc
NativeStar <z2858470935@163.com>
2026-04-11 23:25:52 +0800

    更新内置apk及版本号

--- shared/src/const/ApplicationVersion.ts
+++ shared/src/const/ApplicationVersion.ts
@@ -1,5 +1,5 @@
 const ApplicationVersion={
-    APPLICATION_VERSION_CODE:8,
-    APPLICATION_VERSION_NAME:"1.1.2",
+    APPLICATION_VERSION_CODE:9,
+    APPLICATION_VERSION_NAME:"1.1.3",
 } as const;
 export default ApplicationVersion;
