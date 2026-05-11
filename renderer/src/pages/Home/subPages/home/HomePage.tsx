57e03f14b5e503dabba556d952ec4ca5d1788926
NativeStar <z2858470935@163.com>
2026-05-11 20:38:54 +0800

    主页添加已连接时间显示

--- renderer/src/pages/Home/subPages/home/HomePage.tsx
+++ renderer/src/pages/Home/subPages/home/HomePage.tsx
@@ -104,7 +104,7 @@ export default function HomePage({ hidden, applicationStates, applicationStatesD
             <ActiveNotifications className="top-[45.5%] left-[45%]" />
             <ApplicationStatesBar states={applicationStates} className="top-[9%] right-[2.3%]" dispatch={applicationStatesDispatch} />
             <MediaControl className="right-[15%] top-[9.5%]" />
-            <AudioForwardPanel className="top-[73.5%] left-[10%]" />
+            <AudioForwardPanel className="top-[78.5%] left-[10%]" />
             <FabMenu className="bottom-[2%] right-[2%]" />
         </div>
     )
--- renderer/src/pages/Home/subPages/home/components/DeviceStatePanel.tsx
+++ renderer/src/pages/Home/subPages/home/components/DeviceStatePanel.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useRef, useState } from "react"
 import { twMerge } from "tailwind-merge"
+import { time2str } from "~/utils"
 
-export interface DeviceState{
+export interface DeviceState {
     memoryUsage: number,
     batteryTemperature: number,
     batteryLevel: number,
@@ -11,26 +13,41 @@ interface DeviceStateBarProps {
     state: DeviceState
     className?: string
 }
-export default function DeviceStatePanel({ state ,className}: DeviceStateBarProps) {
+let timer: number;
+export default function DeviceStatePanel({ state, className }: DeviceStateBarProps) {
+    const [currentTime, setCurrentTime] = useState(Date.now());
+    const initTime = useRef(Date.now());
+    useEffect(() => {
+        timer = setInterval(() => {
+            setCurrentTime(Date.now());
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        }
+    }, [])
     return (
-        <mdui-card className={twMerge("fixed flex flex-col h-[25%] w-[34%]",className)}>
+        <mdui-card className={twMerge("fixed flex flex-col h-[30%] w-[34%]", className)}>
             <small className="text-[gray] ml-1 mt-1">设备状态</small>
-            <mdui-divider/>
+            <mdui-divider />
             <div className="flex mt-1.5 ml-1">
                 <mdui-icon name="memory" />
                 <small className="text-[gray] mt-0.5">内存占用:   {Math.ceil(state.memoryUsage)}%</small>
             </div>
             <div className="flex mt-1.5 ml-1">
                 <mdui-icon name="thermostat" />
-                <small className="text-[gray] mt-0.5">电池温度:   {state.batteryTemperature/10}°C</small>
+                <small className="text-[gray] mt-0.5">电池温度:   {state.batteryTemperature / 10}°C</small>
             </div>
             <div className="flex mt-1.5 ml-1">
-                <mdui-icon name={state.charging?"battery_charging_full":"battery_full"} />
-                <small className="text-[gray] mt-0.5">电池电量:   {state.batteryLevel}% {state.charging&&"(充电中)"}</small>
+                <mdui-icon name={state.charging ? "battery_charging_full" : "battery_full"} />
+                <small className="text-[gray] mt-0.5">电池电量:   {state.batteryLevel}% {state.charging && "(充电中)"}</small>
             </div>
             <div className="flex mt-1.5 ml-1">
                 <mdui-icon name="wifi" />
                 <small className="text-[gray] mt-0.5">通讯延迟:   {state.latency}ms</small>
+            </div>
+            <div className="flex mt-1.5 ml-1">
+                <mdui-icon name="access_time" />
+                <small className="text-[gray] mt-0.5">连接时间:   {time2str((currentTime - initTime.current) / 1000)}</small>
             </div>
         </mdui-card>
     )
