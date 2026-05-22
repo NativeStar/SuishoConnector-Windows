f36cfd5b819ed36e0d951d6308483ff5e3ce39e6
NativeStar <z2858470935@163.com>
2026-05-23 00:39:43 +0800

    透明度滑块标签显示百分号

--- renderer/src/pages/Home/subPages/setting/SettingPage.tsx
+++ renderer/src/pages/Home/subPages/setting/SettingPage.tsx
@@ -59,7 +59,7 @@ export default function SettingPage({ hidden }: SettingPageProps) {
                     <SettingItemSwitch title="自动检查更新" desc="在连接设备后联网检查软件更新" icon="update" configs={applicationConfig} configKey="autoCheckUpdate" setConfig={ipc.setConfig} />
                     <SettingItemSwitch title="注册系统文件右键菜单" desc="在系统菜单中快捷将文件通过互传方式发送到手机" icon="menu_open" configKey="enableFileContextMenu" configs={applicationConfig} setConfig={ipc.setConfig} />
                     <SettingItemSwitch title="窗口置顶" desc="将应用窗口始终至于顶层 避免被其他软件窗口覆盖" icon="vertical_align_top" configs={applicationConfig} configKey="windowAlwaysOnTop" setConfig={ipc.setConfig} />
-                    <SettingItemSlider title="窗口不透明度" desc="修改窗口的不透明度(为避免影响扫码 该选项对连接页无效)" icon="opacity" configs={applicationConfig} configKey="windowOpacity" setConfig={ipc.setConfig} step={1} min={10} max={100} />
+                    <SettingItemSlider title="窗口不透明度" desc="修改窗口的不透明度(为避免影响扫码 该选项对连接页无效)" icon="opacity" configs={applicationConfig} configKey="windowOpacity" setConfig={ipc.setConfig} step={1} min={10} max={100} labelFormatter={(value)=>`${value}%`}/>
                     <mdui-list-subheader className="ml-5 h-10 font-bold">数据互传</mdui-list-subheader>
                     <SettingItemSwitch title="接收到重名文件时删除旧文件" desc="否则在新文件名中追加时间戳以继续接收" icon="downloading" configs={applicationConfig} configKey="deleteTransmitConflictFile" setConfig={ipc.setConfig} />
                     <SettingItemSwitch title="删除消息时确认" icon="playlist_remove" desc="删除消息时是否弹出二次确认对话框" configs={deviceConfig} configKey="deleteTransmitMessageConfirm" setConfig={wrappedSetDeviceConfig} />
--- renderer/src/pages/Home/subPages/setting/components/SettingItemSlider.tsx
+++ renderer/src/pages/Home/subPages/setting/components/SettingItemSlider.tsx
@@ -1,5 +1,8 @@
-import { useCallback, useEffect, useState } from "react"
-
+import { useCallback, useEffect, useRef, useState } from "react"
+type MduiSliderElement = HTMLElement & {
+    value: number;
+    labelFormatter: (value: number) => string;
+};
 interface SettingItemSliderProps {
     icon: string
     title: string
@@ -10,29 +13,36 @@ interface SettingItemSliderProps {
     configKey: string,
     setConfig: (key: string, value: string | number | boolean) => void
     disabled?: boolean,
-    min?:number,
-    max?:number,
-    step?:number,
+    min?: number,
+    max?: number,
+    step?: number,
+    labelFormatter?: (value: number) => string
 }
-export default function SettingItemSlider({icon,configKey,title,desc,configs,setConfig,className,disabled,onChange,max,min,step}: SettingItemSliderProps) {
-    const [value,setValue]=useState(0);
-    useEffect(()=>{
-        setValue(configs[configKey] as number??100);
-    },[configs])
-    const internalOnChange=useCallback((e: React.ChangeEvent<HTMLInputElement,HTMLInputElement>)=>{
-        const newValue=parseInt(e.target.value);
-        if(isNaN(newValue)){
+export default function SettingItemSlider({ icon, configKey, title, desc, configs, setConfig, className, disabled, onChange, max, min, step ,labelFormatter}: SettingItemSliderProps) {
+    const [value, setValue] = useState(0);
+    const sliderRef = useRef<MduiSliderElement>(null);
+    useEffect(() => {
+        if (sliderRef.current&&labelFormatter) {
+            sliderRef.current.labelFormatter = labelFormatter
+        }
+    }, [])
+    useEffect(() => {
+        setValue(configs[configKey] as number ?? 100);
+    }, [configs])
+    const internalOnChange = useCallback((e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        const newValue = parseInt(e.target.value);
+        if (isNaN(newValue)) {
             return
         }
-        if(onChange?.(newValue)===false){
+        if (onChange?.(newValue) === false) {
             e.preventDefault();
         }
-        setConfig(configKey,newValue);
+        setConfig(configKey, newValue);
         setValue(newValue);
-    },[]);
+    }, []);
     return (
         <mdui-list-item icon={icon} headline={title} description={desc} className={className}>
-            <mdui-slider min={min} max={max} step={step} disabled={disabled} value={value} className="w-55 right-1.5" slot="end-icon" onChange={internalOnChange}/>
+            <mdui-slider ref={sliderRef} min={min} max={max} step={step} disabled={disabled} value={value} className="w-55 right-1.5" slot="end-icon" onChange={internalOnChange} />
         </mdui-list-item>
     )
 }
